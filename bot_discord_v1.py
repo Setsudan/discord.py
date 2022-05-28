0a2bc26a4788dee2018f69963360b1c6132de86e
daiiruin <arsene03092002@hotmail.com>
2022-05-28 15:04:37 +0200

    Some comments

--- bot_discord_v1.py
+++ bot_discord_v1.py
@@ -2,6 +2,9 @@ import discord
 from discord.ext import commands
 
 client = commands.Bot(command_prefix="!", description="Bot Daiiruin")
+
+### Class de l'arbre ###
+
 class Node :
     def __init__(self, question, keyword, list_child_node):
         self.question = question
@@ -15,15 +18,19 @@ class Node :
             if child.keyword in txt:
                 child.user_response()
 
+### Notre arbre ###
+
 first_node =Node("Salut, je suis le bot qui va t'aider !\nOn va voir ce qu'on peut faire...\nTu veut un tuto ou une documentation ?" , "start",
 [Node("Sur quel language tu as besoin d'un tuto?" , "tuto" , [Node("Voilà ton tuto pour html !","html",["https://www.youtube.com/watch?v=qsbkZ7gIKnc"]), Node("Voilà ton tuto pour python !","python",["https://www.youtube.com/watch?v=oUJolR5bX6g"])]),
 Node("Sur quel language tu as besoin d'une documentation?" , "documentation" , [Node("Voilà ta documentation pour html !","html",["https://developer.mozilla.org/fr/docs/Web/HTML"]),Node("Voilà ta documentation pour python !","python",["https://docs.python.org/fr/3/"])])])
 
-client = commands.Bot(command_prefix="!")
+### Attendre le print dans la console pour être sûr que tout est bon ###
 
 @client.event
 async def on_ready():
     print("Le bot est prêt !")
+
+### Juste des tests ###
 
 @client.command()
 async def test(ctx):
@@ -31,26 +38,32 @@ async def test(ctx):
     await ctx.send(first_node.list_child_node[0].list_child_node[0].list_child_node)
     await ctx.send(first_node.list_child_node[0].list_child_node[0].keyword)
 
+### Commande d'aide pour un language de programmation ( "!" )###
+
 @client.command()
 async def aide(ctx):
     await ctx.send(first_node.question)
 
+    ### Fonction pour voir si le message viens bien de la personne en question et pas une autre ###
     def check(message):
         return message.author == ctx.message.author
     
+    ### On attend la réponse de l'utilisateur
     reponse = await client.wait_for("message", check = check)
+    ### On regarde si le message est dans la liste contenant le mot clé tuto
     if reponse.content in first_node.list_child_node[0].keyword:
         await ctx.send(first_node.list_child_node[0].question)
+        ### On attend encore la réponse de l'utilisateur
         reponse2 = await client.wait_for("message", check = check)
         print(reponse2.content)
-        print("ok suivant")
-
+        ### On fait une boucle for qui vérifie si la réponse est dans la longue liste des language contenant le mot clé de celui-ci
         for i in range(0,2):
             if reponse2.content in first_node.list_child_node[0].list_child_node[i].keyword:
                 await ctx.send(first_node.list_child_node[0].list_child_node[i].question)
                 await ctx.send(first_node.list_child_node[0].list_child_node[i].list_child_node)
     
-    
+    ### Exactement même procésus que pour le tuto mais pour la documentation
+
     elif reponse.content in first_node.list_child_node[1].keyword:
         await ctx.send(first_node.list_child_node[1].question)
         reponse2 = await client.wait_for("message", check = check)
@@ -61,6 +74,8 @@ async def aide(ctx):
             if reponse2.content in first_node.list_child_node[1].list_child_node[i].keyword:
                 await ctx.send(first_node.list_child_node[1].list_child_node[i].question)
                 await ctx.send(first_node.list_child_node[1].list_child_node[i].list_child_node)
+
+### Juste une fonction qui permettera de supprimer des messages ( ici 40... )
 
 @client.event
 async def on_message(message):
